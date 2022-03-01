6822cb4643399e11f8d8a51ce2fc93ad3f0aa4e9
ivan Wellesz <ivanw@orange.fr>
2022-03-01 17:35:01 +0100

    nodejs --inspect (debugging from chrome)

--- server.sh
+++ server.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #┌────────────────────────────────────────────────────────────────────────────┐
-#│ server.sh                                              _TAG(211022:17h:53) │
+#│ server.sh                                              _TAG(211025:18h:01) │
 #└────────────────────────────────────────────────────────────────────────────┘
 function signal_trap()                { echo "*** SIGNAL TRAP ***"; press_any_key_to_terminate; }
 function press_any_key_to_terminate() { read -p "PRESS ANY KEY TO TERNINATE .. (EXIT_CODE $EXIT_CODE)...";  exit $EXIT_CODE; }
 trap signal_trap SIGINT
 
  EXIT_CODE=-1
- cd `pwd` && node ./SERVER/server.js
+ cd `pwd` && node --inspect ./SERVER/server.js
  EXIT_CODE=$?
 
  if [ $EXIT_CODE != 0 ]; then echo "EXIT_CODE=[$EXIT_CODE]"; else echo "...done"; fi
